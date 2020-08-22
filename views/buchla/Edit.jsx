a45113ae1d688e466ebfe519fc4d9145beab17e9
JamesLeggero <james.m.leggero@gmail.com>
2020-08-22 18:00:56 -0400

    edit page styled

---
+++ views/buchla/Edit.jsx
@@ -0,0 +1,131 @@
+const React = require('react')
+const Default = require('../components/Default.jsx')
+const BuchlaLayout = require('../components/BuchlaLayout.jsx')
+
+class Edit extends React.Component {
+    render(){
+        const { _id, model, name, modFunction, description, img, audioOutput, voltageMax, voltageMin, acceptsNegative, isModule } = this.props.buchla
+        return(
+
+<Default>
+                <BuchlaLayout>
+                    <div >
+                        <div className='columns' style={{height: '100%', margin: '50px'}}>
+                            <div className='column is-half'>
+                        
+
+                <form style={{maxWidth: '100%'}} action={`/buchla/${_id}?_method=PUT`} method='POST'>
+                    <div className='field'>
+                        <label className='label'>Model Number</label>
+                        <div className='control'>
+                        <input type='text' name='model' value={model}/>
+                        </div>
+                    </div>
+
+                    <div className='field'>
+                        <label className='label'>Name</label>
+                        <div className='control'>
+                        <input type='text' name='name' value={name}/>
+                        </div>
+                    </div>
+
+                        <div className="field">
+                            <label htmlFor='modFunction' className="label">Primary Function</label>
+                            <div className="control">
+                                <div className="select">
+
+                                    <select name='modFunction' value={modFunction}>
+                                        <option value='Oscillator'>Oscillator</option>
+                                        <option value='Filter'>Filter</option>
+                                        <option value='Envelope'>Envelope</option>
+                                        <option value='VCA'>VCA</option>
+                                        <option value='LFO'>LFO</option>
+                                        <option value='OTHER'>Other</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='field'>
+                            <label className='label'>Description</label>
+                            <div className='control'>
+                                <textarea className='textarea' name='description' value={description}>
+                                
+                                </textarea>
+                            </div>
+                        </div>
+
+                        <div className='field'>
+                        <label className='label'>Image Source</label>
+                        <div className='control'>
+                        <input type='text' name='img' value={img}/>
+                        </div>
+                    </div>
+
+                    <div className="field">
+                            <label htmlFor='audioOutput' className="label">Audio I/O</label>
+                            <div className="control">
+                                <div className="select">
+
+                                    <select name='audioOutput' value={audioOutput}>
+                                        <option value='Banana'>Banana</option>
+                                        <option value='1/8 Inch'>1/8 Inch</option> 
+                                        <option value='Other'>Other</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                    <div className='field'>
+                        <label className='label'>Max Voltage</label>
+                        <div className='control'>
+                        <input type='text' name='voltageMax' value={voltageMax}/>
+                        </div>
+                    </div>
+                    
+                    <div className='field'>
+                        <label className='label'>Min Voltage</label>
+                        <div className='control'>
+                        <input type='text' name='voltageMin' value={voltageMin}/>
+                        </div>
+                    </div>
+
+                    <div className='field'>
+                        <div className='control'>
+                            <label className='checkbox'>
+                                <input type='checkbox' name='acceptsNegative' checked={acceptsNegative ? 'checked' : ''}/>Accepts Negative Voltage?
+                            </label>
+                        </div>
+                    </div>
+
+                    <div className='field'>
+                        <div className='control'>
+                            <label className='checkbox'>
+                                <input type='checkbox' name='isModule' checked={acceptsNegative ? 'checked' : ''} />Is a Module?
+                            </label>
+                        </div>
+                    </div>
+
+
+
+
+                    <input type='submit' name='' value='Update Buchla'/>
+                
+            </form>
+            </div>
+            <div className='column is-half'>
+                <img src={img} />
+
+                </div>
+            </div>
+            </div>
+            </BuchlaLayout>
+            </Default>
+
+
+
+            )
+        }
+    }
+    
+    module.exports = Edit
--- views/buchla/New.jsx
+++ views/buchla/New.jsx
@@ -7,7 +7,7 @@ class New extends React.Component {
         return(
             <Default>
                 <BuchlaLayout>
-                    <div style={{height: '100vh'}}>
+                    <div style={{height: '100vh', margin: '50px', marginBottom: '-50px'}}>
 
                 <form style={{maxWidth: '33%'}} action='/buchla' method='POST'>
                     <div className='field'>
@@ -44,7 +44,9 @@ class New extends React.Component {
                         <div className='field'>
                             <label className='label'>Description</label>
                             <div className='control'>
-                                <input  type='text' className='textarea' name='description' />
+                            <textarea className='textarea' name='description'>
+                                
+                                </textarea>
                             </div>
                         </div>
 
@@ -107,28 +109,7 @@ class New extends React.Component {
             </form>
 
 
-                    
-
-           
-        {/* <div>
-            <h1>New Buchla Creation Page</h1>
-            <form action='/buchla' method='POST'>
-                <h3>Model</h3><input type='text' name='model'/>
-                <h3>Name</h3><input type='text' name='name'/>
-                <h3>Function</h3><input type='text' name='modFunction'/>
-
-                <h3>Description</h3><input type='text' name='description'/>
-                <h3>Image Source</h3><input type='text' name='img'/>
-                <h3>Max Voltage</h3><input type='text' name='voltageMax'/>
-
-                <h3>Min Voltage</h3><input type='text' name='voltageMin'/>
-                <h3>Accepts Negative Voltage?</h3><input type='checkbox' name='acceptsNegative'/>
-                <h3>Is Module?</h3><input type='checkbox' name='isModule' checked/><br/>
                 
-                <input type='submit' name='' value='Create New Buchla'/>
-                
-            </form>
-        </div> */}
         </div>
 
 
